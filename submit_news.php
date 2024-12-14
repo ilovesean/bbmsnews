9e02c4eac51569f7cd5f7a82cd5f6cb5d44cc923
ilovesean <jhandy5700@inst.hcpss.org>
2024-12-13 19:05:42 -0500

    Update submit_news.php

--- submit_news.php
+++ submit_news.php
@@ -1,1 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "school_news";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $title = $_POST["title"];
+    $content = $_POST["content"];
+    $image = $_POST["image"];
+
+    $sql = "INSERT INTO news (title, content, image) VALUES ('$title', '$content', '$image')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
 
